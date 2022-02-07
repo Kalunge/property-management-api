acc1e3cf5d5f5f940bc2e43814d3d3982cd8426e
kalunge <muthomititus+github@gmail.com>
2022-02-07 16:31:09 +0100

    Add User Resources

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,4 +1,4 @@
-class UsersController < ApplicationController
+class Api::V1::UsersController < ApplicationController
   before_action :set_user, only: [:show, :update, :destroy]
 
   # GET /users
@@ -49,3 +49,4 @@ class UsersController < ApplicationController
       params.fetch(:user, {})
     end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  resources :users
+  namespace :api do
+    namespace :v1 do
+      resources :users
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
