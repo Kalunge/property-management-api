53777674398e88fe1addc7d99908255fb4083be3
kalunge <muthomititus+github@gmail.com>
2022-02-07 16:34:54 +0100

    Add Tenant And Reservations CRUD operations

---
+++ app/controllers/tenants_controller.rb
@@ -0,0 +1,51 @@
+class TenantsController < ApplicationController
+  before_action :set_tenant, only: [:show, :update, :destroy]
+
+  # GET /tenants
+  def index
+    @tenants = Tenant.all
+
+    render json: @tenants
+  end
+
+  # GET /tenants/1
+  def show
+    render json: @tenant
+  end
+
+  # POST /tenants
+  def create
+    @tenant = Tenant.new(tenant_params)
+
+    if @tenant.save
+      render json: @tenant, status: :created, location: @tenant
+    else
+      render json: @tenant.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /tenants/1
+  def update
+    if @tenant.update(tenant_params)
+      render json: @tenant
+    else
+      render json: @tenant.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /tenants/1
+  def destroy
+    @tenant.destroy
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_tenant
+      @tenant = Tenant.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def tenant_params
+      params.fetch(:tenant, {})
+    end
+end
---
+++ test/controllers/tenants_controller_test.rb
@@ -0,0 +1,38 @@
+require "test_helper"
+
+class TenantsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @tenant = tenants(:one)
+  end
+
+  test "should get index" do
+    get tenants_url, as: :json
+    assert_response :success
+  end
+
+  test "should create tenant" do
+    assert_difference('Tenant.count') do
+      post tenants_url, params: { tenant: {  } }, as: :json
+    end
+
+    assert_response 201
+  end
+
+  test "should show tenant" do
+    get tenant_url(@tenant), as: :json
+    assert_response :success
+  end
+
+  test "should update tenant" do
+    patch tenant_url(@tenant), params: { tenant: {  } }, as: :json
+    assert_response 200
+  end
+
+  test "should destroy tenant" do
+    assert_difference('Tenant.count', -1) do
+      delete tenant_url(@tenant), as: :json
+    end
+
+    assert_response 204
+  end
+end
