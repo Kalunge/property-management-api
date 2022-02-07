0e715a45f20e4d5c3d2fb0756f61199af2dce74b
kalunge <muthomititus+github@gmail.com>
2022-02-07 16:33:54 +0100

    Add Block and apartment CRUD operations

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users
+      resources :blocks
+      resources :apartments
+      resources :tenants
+      resources :reservations
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
---
+++ test/controllers/blocks_controller_test.rb
@@ -0,0 +1,38 @@
+require "test_helper"
+
+class BlocksControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @block = blocks(:one)
+  end
+
+  test "should get index" do
+    get blocks_url, as: :json
+    assert_response :success
+  end
+
+  test "should create block" do
+    assert_difference('Block.count') do
+      post blocks_url, params: { block: {  } }, as: :json
+    end
+
+    assert_response 201
+  end
+
+  test "should show block" do
+    get block_url(@block), as: :json
+    assert_response :success
+  end
+
+  test "should update block" do
+    patch block_url(@block), params: { block: {  } }, as: :json
+    assert_response 200
+  end
+
+  test "should destroy block" do
+    assert_difference('Block.count', -1) do
+      delete block_url(@block), as: :json
+    end
+
+    assert_response 204
+  end
+end
