3099787d418dd343e4fc68c0df63d20f6da581b7
kalunge <muthomititus+github@gmail.com>
2022-02-11 13:25:04 +0100

    Add block representer

--- spec/requests/blocks_spec.rb
+++ spec/requests/blocks_spec.rb
@@ -1,11 +1,19 @@
 require 'rails_helper'
 
 describe 'Blocks API', type: :request do
-  let!(:user_one) {FactoryBot.create(:user, name: "Grace Acibi")}
-  let!(:block_one) {FactoryBot.create(:block, name: "Macom Apartments", location: "KdFarmaco Kikuyu", user_id:user_one.id)}
-  describe "GET /blocks" do
-    it "returns all blocks" do
-      get "/api/v1/blocks"
+  let!(:user_one) { FactoryBot.create(:user, name: 'Grace Acibi') }
+  let!(:block_one) do
+    FactoryBot.create(:block, name: 'Macom Apartments', location: 'KdFarmaco Kikuyu', user_id: user_one.id)
+  end
+  let!(:block_two) do
+    FactoryBot.create(:block, name: 'Waringa Apartments', location: 'KdFarmaco Kikuyu', user_id: user_one.id)
+  end
+  let!(:block_three) do
+    FactoryBot.create(:block, name: 'Kidron Valley Apartments', location: 'KdFarmaco Kikuyu', user_id: user_one.id)
+  end
+  describe 'GET /blocks' do
+    it 'returns all blocks' do
+      get '/api/v1/blocks'
 
       expect(response).to have_http_status(:success)
       expect(JSON.parse(response.body)).to eq([])
