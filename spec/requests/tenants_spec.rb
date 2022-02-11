2db932e60a687c79ad3315ff9feb1b07f31b3031
kalunge <muthomititus+github@gmail.com>
2022-02-11 16:35:52 +0100

    Add tenant factorybot and get all tenants tests

---
+++ spec/requests/tenants_spec.rb
@@ -0,0 +1,22 @@
+require "rails_helper"
+
+describe "API /tenants", type: :request do
+  let!(:my_tenant) {FactoryBot.create(:tenant, name:"Titus Kalunge", email:"tenant@mail.com", deposit:180000)}
+
+  describe "GET /Tenants" do
+    it "Gets all tenants" do
+      get "/api/v1/tenants"
+
+      expect(response).to have_http_status(:success)
+      expect(JSON.parse(response.body)).to eq([
+        {
+          "id" => my_tenant.id,
+          "email" => my_tenant.email,
+          "phone" => my_tenant.phone,
+          "name" => my_tenant.name,
+          "deposit" => my_tenant.deposit
+        }
+      ])
+    end
+  end
+end
