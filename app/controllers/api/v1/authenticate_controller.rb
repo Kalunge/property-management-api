540453e06f7fc209562bc40a5dc32921abfaf30b
kalunge <muthomititus+github@gmail.com>
2022-02-14 12:59:54 +0100

    Resolve rubocop errors

--- app/controllers/api/v1/authenticate_controller.rb
+++ app/controllers/api/v1/authenticate_controller.rb
@@ -3,26 +3,24 @@ class Api::V1::AuthenticateController < ApplicationController
   rescue_from ActionController::ParameterMissing, with: :param_missing
   rescue_from AuthenticationError, with: :handle_unauthenticated
   def create
-
     raise AuthenticationError unless user.authenticate(params.require(:password))
+
     token = AuthenticationTokenService.encode(user.id)
 
-    render json: {token: token}, status: :created
+    render json: { token: token }, status: :created
   end
-
 
   private
 
-   def user
+  def user
     @user ||= User.find_by(name: params.require(:name))
   end
 
   def param_missing
-    render json: {errors: "param is missing or the value is empty: username/password"}, status: :unprocessable_entity
+    render json: { errors: 'param is missing or the value is empty: username/password' }, status: :unprocessable_entity
   end
 
   def handle_unauthenticated
-    render json: {errors: "Wrong Email or password"}, status: :unprocessable_entity
+    render json: { errors: 'Wrong Email or password' }, status: :unprocessable_entity
   end
-
 end
