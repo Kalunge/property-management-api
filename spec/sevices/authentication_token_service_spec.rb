ac9bdb3031d8116e58ed681cc25b5a84e8ba8fc3
kalunge <muthomititus+github@gmail.com>
2022-02-14 11:30:59 +0100

    Add authentication token service logic

--- spec/sevices/authentication_token_service_spec.rb
+++ spec/sevices/authentication_token_service_spec.rb
@@ -1,15 +1,12 @@
 require 'rails_helper'
 
-describe AuthenticatioTokenService do
+describe AuthenticationTokenService do
   describe '.encode' do
     let(:token) { described_class.encode(123) }
 
     it 'returns a token' do
-      decoded_token = JWT.decoded_token, described_class::HMAC_SECRET, true,
-                      { algorithm: described_class::ALGORTHIM_TYPE }
-      expect(decoded_token).to eq([
-                                    { 'user_id' => 123 }, { 'alg' => 'HS256' }
-                                  ])
+      decoded_token = described_class.decode(token)
+      expect(decoded_token).to eq(123)
     end
   end
 end
