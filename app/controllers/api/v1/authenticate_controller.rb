8f74d39d177618e3c141e8786932642e7b825620
kalunge <muthomititus+github@gmail.com>
2022-02-14 12:59:18 +0100

    Add authentication tests for wrong and missing params

--- app/controllers/api/v1/authenticate_controller.rb
+++ app/controllers/api/v1/authenticate_controller.rb
@@ -1,5 +1,10 @@
 class Api::V1::AuthenticateController < ApplicationController
+  class AuthenticationError < StandardError; end
+  rescue_from ActionController::ParameterMissing, with: :param_missing
+  rescue_from AuthenticationError, with: :handle_unauthenticated
   def create
+
+    raise AuthenticationError unless user.authenticate(params.require(:password))
     token = AuthenticationTokenService.encode(user.id)
 
     render json: {token: token}, status: :created
@@ -10,6 +15,14 @@ class Api::V1::AuthenticateController < ApplicationController
 
    def user
     @user ||= User.find_by(name: params.require(:name))
+  end
+
+  def param_missing
+    render json: {errors: "param is missing or the value is empty: username/password"}, status: :unprocessable_entity
+  end
+
+  def handle_unauthenticated
+    render json: {errors: "Wrong Email or password"}, status: :unprocessable_entity
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   has_many :reservations
   has_many :blocks
   has_many :apartments
+
+  validates :name, presence: true
+  validates :password, presence: true
 end
